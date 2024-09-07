fc1f5064444d332359f66d74646b14d5088ed624
CloudStoned <cloudneo0719@gmail.com>
2024-09-07 12:51:20 +0800

    Update android project implement mediapipe

--- asl_recog/app/src/main/java/com/example/asl_recog/MainActivity.java
+++ asl_recog/app/src/main/java/com/example/asl_recog/MainActivity.java
@@ -2,6 +2,13 @@ package com.example.asl_recog;
 
 import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.ImageFormat;
+import android.graphics.Matrix;
+import android.graphics.Rect;
+import android.graphics.YuvImage;
+import android.media.Image;
 import android.os.Bundle;
 import android.util.Log;
 import android.util.Size;
@@ -10,9 +17,11 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.OptIn;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.camera.core.Camera;
 import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ExperimentalGetImage;
 import androidx.camera.core.ImageAnalysis;
 import androidx.camera.core.ImageProxy;
 import androidx.camera.core.Preview;
@@ -22,6 +31,10 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import com.google.common.util.concurrent.ListenableFuture;
+import com.google.mediapipe.formats.proto.LandmarkProto;
+import com.google.mediapipe.solutions.hands.Hands;
+import com.google.mediapipe.solutions.hands.HandsOptions;
+import com.google.mediapipe.solutions.hands.HandsResult;
 
 import org.pytorch.IValue;
 import org.pytorch.LiteModuleLoader;
@@ -30,11 +43,13 @@ import org.pytorch.Tensor;
 import org.pytorch.torchvision.TensorImageUtils;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
@@ -51,13 +66,15 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView textView;
     private Module module;
-    private List<String> classes;
+    private List<String> CLASSES;
     private final Executor executor = Executors.newSingleThreadExecutor();
 
     private TextView combinedLettersTextView;
     private Button combineLettersButton;
     private Button clearButton;
     private StringBuilder combinedLetters = new StringBuilder();
+
+    private Hands hands;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -72,6 +89,17 @@ public class MainActivity extends AppCompatActivity {
 
         combineLettersButton.setOnClickListener(v -> combineLetters());
         clearButton.setOnClickListener(v -> clearCombinedLetters());
+
+        // Initialize MediaPipe
+        HandsOptions handsOptions = HandsOptions.builder()
+                .setStaticImageMode(false)
+                .setMaxNumHands(1)
+                .setRunOnGpu(true).build();
+        hands = new Hands(this, handsOptions);
+
+        // Set up Result Listenetr
+        hands.setResultListener(this::analyzeHandsResult);
+        hands.setErrorListener(((message, e) ->  Log.e(TAG, "Mediapipe Hands Error: " + message)));
 
         if (checkPermissions()) {
             initializeApp();
@@ -90,7 +118,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initializeApp() {
-        classes = loadClasses("classes.txt");
+        CLASSES = loadClasses("classes.txt");
         loadTorchModule("2_CLASSES_MODEL.ptl");
         cameraProviderFuture = ProcessCameraProvider.getInstance(this);
         cameraProviderFuture.addListener(() -> {
@@ -116,6 +144,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @OptIn(markerClass = ExperimentalGetImage.class)
     private void startCamera(@NonNull ProcessCameraProvider cameraProvider) {
         Preview preview = new Preview.Builder().build();
         CameraSelector cameraSelector = new CameraSelector.Builder()
@@ -128,7 +157,11 @@ public class MainActivity extends AppCompatActivity {
                 .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
                 .build();
 
-        imageAnalysis.setAnalyzer(executor, this::analyzeImage);
+        imageAnalysis.setAnalyzer(executor, image -> {
+            Bitmap bitmap = imageToBitmap(image.getImage());
+            hands.send(bitmap);
+            image.close();
+        });
 
         Camera camera = cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageAnalysis);
     }
@@ -154,35 +187,29 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void analyzeImage(@NonNull ImageProxy image) {
-        int rotation = image.getImageInfo().getRotationDegrees();
-        try {
-            int cropSize = 224;
-            float[] mean = {0.485f, 0.456f, 0.406f};
-            float[] std = {0.229f, 0.224f, 0.225f};
-
-            @SuppressLint("UnsafeOptInUsageError")
-            Tensor inputTensor = TensorImageUtils.imageYUV420CenterCropToFloat32Tensor(
-                    image.getImage(), rotation, cropSize, cropSize,
-                    mean, std
-            );
-
-            Tensor outputTensor = module.forward(IValue.from(inputTensor)).toTensor();
-
-
-            float[] scores = outputTensor.getDataAsFloatArray();
-
-            int maxScoreIdx = 0;
-            float maxScore = scores[0];
-            for (int i = 1; i < scores.length; i++) {
-                if (scores[i] > maxScore) {
-                    maxScore = scores[i];
-                    maxScoreIdx = i;
-                }
+    private void analyzeHandsResult(HandsResult handsResult)
+    {
+        try
+        {
+            if (handsResult.multiHandLandmarks().isEmpty())
+            {
+                runOnUiThread(() -> textView.setText("No Hand Detected"));
             }
 
-            if (maxScoreIdx >= 0 && maxScoreIdx < classes.size()) {
-                String classResult = classes.get(maxScoreIdx);
+            // Extract Landmarks
+            List<LandmarkProto.NormalizedLandmark> landmarks = handsResult.multiHandLandmarks().get(0).getLandmarkList();
+            float[] landmarkArray = convertLandmarksToArray(landmarks);
+
+            // Create Input Tensor
+            Tensor inputTensor = Tensor.fromBlob(landmarkArray, new long[]{1, 63});
+
+            // Run Inference
+            Tensor outputTensor = module.forward(IValue.from(inputTensor)).toTensor();
+            float[] scores = outputTensor.getDataAsFloatArray();
+            final int maxScoreIdx = argmax(scores);
+
+            if (maxScoreIdx >= 0 && maxScoreIdx < CLASSES.size()) {
+                String classResult = CLASSES.get(maxScoreIdx);
                 Log.d(TAG, "Detected - " + classResult);
                 runOnUiThread(() -> {
                     textView.setText(classResult);
@@ -192,12 +219,34 @@ public class MainActivity extends AppCompatActivity {
             } else {
                 Log.e(TAG, "Index out of bounds for class labels");
             }
-
-        } catch (Exception e) {
-            Log.e(TAG, "Error analyzing image", e);
-        } finally {
-            image.close();
         }
+        catch (Exception e){
+            Log.e(TAG, "Error analyzing image", e);
+        }
+    }
+
+    private int argmax(float[] array) {
+        int maxIdx = 0;
+        float maxVal = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > maxVal) {
+                maxVal = array[i];
+                maxIdx = i;
+            }
+        }
+        return maxIdx;
+    }
+
+    private float[] convertLandmarksToArray(List<LandmarkProto.NormalizedLandmark> landmarks)
+    {
+        float[] landmarkArray = new float[landmarks.size() * 3];
+        for (int i = 0; i < landmarks.size(); i++) {
+            LandmarkProto.NormalizedLandmark landmark = landmarks.get(i);
+            landmarkArray[i * 3] = landmark.getX();
+            landmarkArray[i * 3 + 1] = landmark.getY();
+            landmarkArray[i * 3 + 2] = landmark.getZ();
+        }
+        return landmarkArray;
     }
 
     private List<String> loadClasses(String fileName) {
@@ -229,6 +278,31 @@ public class MainActivity extends AppCompatActivity {
         combinedLetters.setLength(0);
         combinedLettersTextView.setText("Combined Letters");
     }
+
+    // Convert Image object to Bitmap
+    private Bitmap imageToBitmap(Image image) {
+        Image.Plane[] planes = image.getPlanes();
+        ByteBuffer yBuffer = planes[0].getBuffer();
+        ByteBuffer uBuffer = planes[1].getBuffer();
+        ByteBuffer vBuffer = planes[2].getBuffer();
+
+        int ySize = yBuffer.remaining();
+        int uSize = uBuffer.remaining();
+        int vSize = vBuffer.remaining();
+
+        byte[] nv21 = new byte[ySize + uSize + vSize];
+        yBuffer.get(nv21, 0, ySize);
+        vBuffer.get(nv21, ySize, vSize);
+        uBuffer.get(nv21, ySize + vSize, uSize);
+
+        YuvImage yuvImage = new YuvImage(nv21, android.graphics.ImageFormat.NV21, image.getWidth(), image.getHeight(), null);
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        yuvImage.compressToJpeg(new Rect(0, 0, yuvImage.getWidth(), yuvImage.getHeight()), 75, out);
+
+        byte[] imageBytes = out.toByteArray();
+        return BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.length);
+    }
+
 
 
 
